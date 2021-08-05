257a9f71a970b2a9e009623b9eb9c3117e39ba4e
sayan556 <75530727+sayan556@users.noreply.github.com>
2021-08-05 16:34:14 +0530

    Update instance.tf

--- EC2/instance.tf
+++ EC2/instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "test" {
-  ami           = "ami-09e67e426f25ce0d7"
+  ami           = "ami-0ee02acd56a52998e"
   instance_type = var.instance
   tags = {
     Name        = "test"
